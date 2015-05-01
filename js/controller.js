0b9e48d22ea0988b9ab6c70556c41fb6f2b6e5c9
neeraj87 <neerajjadhav@Neerajs-MacBook-Pro.local>
2015-05-01 11:14:20 -0500

    Color and other bug fixes

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,23 @@
 var $j = jQuery.noConflict(true);
 var clickCount = 0;
-var gameOver = false;
+var gameStart = false;
+var divBackgroundColorsArray = ["#ff8f85", "#2dcc70", "#b1a1c6", "#ff0059", "#a500ff", "#ec26e6", "#f28e61", "#f0f261", "#61e5f2", "#4ff8bc", "#f8eb4f", "#f54ff8", "#c9f84f", "#f8cd4f", "#f8a14f"];
 
 $j(document).ready(function(){	
 	start();
 
 	$j('.row td').click(function(event){
 		var innerVal = $j(this).children('.block').children('.label').text();
-		if(!gameOver) {
-			registerClick(innerVal);
+		var elementId = $j(this).children('.block').children('label').attr('id');
+		if(gameStart) {
+			registerClick(innerVal, elementId);
 		}
 	});
 
 });
 
 function start() {
-	gameOver = false;
+	gameStart = false;
 	clickCount = 0;
 	generateNumbers();
 	timeOut();
@@ -29,6 +31,7 @@ function generateNumbers() {
 			var randLoc = randomiser(40, 0);
 			if ($j('#'+randLoc).html() === '') {
 				$j('#'+randLoc).html(i);
+				$j('#'+randLoc).parent().css('background-color', divBackgroundColorsArray[randomiser(14, 0)]);
 				placed = true;
 			}
 		}
@@ -39,6 +42,7 @@ function reset() {
 	for(var i = 1; i <= 40; i++) {
 		$j('#'+i).html('');
 	}
+	$j('.block').css('background-color', '#c0c0c0');
 }
 
 function modFunction(value1, value2) {
@@ -49,32 +53,25 @@ function randomiser(upperBound, lowerBound) {
     return Math.round(Math.random() * (upperBound - lowerBound));
 }
 
-function registerClick(tdVal) {
+function registerClick(tdVal, elemId) {
 	clickCount++;
 
 	if(clickCount != tdVal) {
-		gameOver = true;
 		$j('.row td .block .label').css('display', 'inline-block');
 		alert('Game over you loose..starting again..');
 		$j('.row td .block .label').css('display', 'block');
 		reset();
 		start();
 	} else {
-		// $('.label').each(function(x){
-		// 	console.log(x);
-		// 	console.log(this);
-		// 	if (x.html() === clickCount) {
-		// 		x.css('display', block);
-		// 	}
-		// })
+		$j('#'+elemId).css('display', 'inline-block');
 	}
-	
 }
 
 function timeOut() {
-	setTimeout(function(){ clearGameBoard() }, 10000);
+	setTimeout(function(){ clearGameBoard() }, 3000);
 }
 
 function clearGameBoard() {
 	$j('.row td .block .label').css('display', 'none');
+	gameStart = true;
 }
